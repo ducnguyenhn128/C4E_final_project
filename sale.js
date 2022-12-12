50d902eb0c799bf49bcaafd60cdc8cbae70d6c4a
ducnguyenhn128 <ducnguyen.dautunhanha@gmail.com>
2022-12-12 21:34:14 +0700

    UPDATE

--- sale.js
+++ sale.js
@@ -1,25 +1,36 @@
 const sale = [
     {
         name: "Áo polo can phối 1.DSTP645",
+        type: "POLO",
         price: "420,000₫",
         link: "https://bucket.nhanh.vn/store/2071/ps/20221012/52351390471_eae4c80956_o__1_.jpg"
     },
 
     {
         name: "Áo polo họa tiết Pattern ngang ngực 1.DSTP621",
+        type: "POLO",
         price: "420,000₫",
         link: "https://bucket.nhanh.vn/store/2071/ps/20221012/52351814650_0621a5a122_o.jpg"
     },
     {
         name:"Áo polo in tràn họa tiết Unit 1.DSTP646",
+        type: "POLO",
         price: "400.000 đ",
         link: "https://bucket.nhanh.vn/store/2071/ps/20221013/52288898508_ed1cdb9c89_o.jpg"
     },
     {
         name:"Áo polo trơn bo kẻ ",
+        type: "POLO",
         price:"380.000 đ",
         link:"https://bucket.nhanh.vn/store/2071/ps/20221012/52307427720_979d4c2935_o.jpg"
+    },
+    {
+        name:"Áo polo trơn bo kẻ ",
+        type: "POLO",
+        price:"400.000 đ",
+        link:"https://bucket.nhanh.vn/store/2071/ps/20221012/52307427720_979d4c2935_o.jpg"
     }
+
 ]
 
 
